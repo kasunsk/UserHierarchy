635c3489527de277c8e70821c1d016d790e011df
Kasun <kasunsk@gmail.com>
2020-06-22 19:14:20 +0800

    Init

---
+++ src/test/java/com/user/hierarchy/HierarchyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.hierarchy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HierarchyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
